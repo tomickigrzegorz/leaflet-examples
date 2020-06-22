20a605f7a16ce1b4f7e5d8b09ac2485ce1903a46
Grzegorz Tomicki <gtomik@gmail.com>
2020-06-22 11:51:16 +0200

    adding shadow to markers

--- 13.svg-markers-width-legends/script.js
+++ 13.svg-markers-width-legends/script.js
@@ -68,7 +68,8 @@ const markers = [
 function colorMarker(color) {
   const svgTemplate = `
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" class="marker">
-      <path stroke="#000000" fill="#${color}" d="M15.938 32S6 17.938 6 11.938C6 .125 15.938 0 15.938 0S26 .125 26 11.875C26 18.062 15.938 32 15.938 32zM16 6c-2.209 0-4 1.791-4 4s1.791 4 4 4 4-1.791 4-4-1.791-4-4-4z"/>
+      <path fill-opacity=".25" d="M16 32s1.427-9.585 3.761-12.025c4.595-4.805 8.685-.99 8.685-.99s4.044 3.964-.526 8.743C25.514 30.245 16 32 16 32z"/>
+      <path fill="#${color}" stroke="#000" d="M15.938 32S6 17.938 6 11.938C6 .125 15.938 0 15.938 0S26 .125 26 11.875C26 18.062 15.938 32 15.938 32zM16 6a4 4 0 100 8 4 4 0 000-8z"/>
     </svg>`;
 
   const icon = L.divIcon({
