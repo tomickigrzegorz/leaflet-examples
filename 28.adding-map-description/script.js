291529654dacaad2d252d46aac7451d3598842e3
Grzegorz Tomicki <gtomik@gmail.com>
2020-08-28 22:56:03 +0200

    adding map description

---
+++ 28.adding-map-description/script.js
@@ -0,0 +1,37 @@
+/**
+ * Adding map description
+ */
+
+// config map
+let config = {
+  minZoom: 7,
+  maxZomm: 18,
+};
+// magnification with which the map will start
+const zoom = 18;
+// co-ordinates
+const lat = 52.2297700;
+const lon = 21.0117800;
+
+// calling map
+const map = L.map('map', config).setView([lat, lon], zoom);
+
+// Used to load and display tile layers on the map
+// Most tile servers require attribution, which you can set under `Layer`
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+// create legend
+const legend = L.control({ position: 'bottomleft' });
+
+legend.onAdd = function () {
+  let div = L.DomUtil.create('div', 'description');
+  L.DomEvent.disableClickPropagation(div);
+  const text = '<b>Lorem Ipsum</b> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book...';
+  div.insertAdjacentHTML('beforeend', text);
+  return div;
+}
+
+legend.addTo(map);
