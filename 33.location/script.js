60301744ebcf2fdcf706e7c2c15f22516b9037de
Grzegorz Tomicki <gtomik@gmail.com>
2020-12-05 08:37:19 +0100

    feat: location

---
+++ 33.location/script.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-undef */
+/**
+ * location
+ */
+
+// config map
+let config = {
+  minZoom: 7,
+  maxZomm: 18,
+};
+// magnification with which the map will start
+const zoom = 18;
+// co-ordinates
+const lat = 52.22977;
+const lon = 21.01178;
+
+// calling map
+const map = L.map('map', config).setView([lat, lon], zoom);
+
+// Used to load and display tile layers on the map
+// Most tile servers require attribution, which you can set under `Layer`
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+map
+  .locate({
+    // https://leafletjs.com/reference-1.7.1.html#locate-options-option
+    setView: true,
+    watch: true,
+    enableHighAccuracy: true,
+  })
+  // if location found show marker and circle
+  .on('locationfound', (e) => {
+    // marker
+    const marker = L.marker([e.latitude, e.longitude]).bindPopup(
+      'Your are here :)'
+    );
+    // circle
+    const circle = L.circle([e.latitude, e.longitude], e.accuracy / 2, {
+      weight: 2,
+      color: 'red',
+      fillColor: 'red',
+      fillOpacity: 0.1,
+    });
+    // add marker
+    map.addLayer(marker);
+    // add circle
+    map.addLayer(circle);
+  })
+  // if error show alert
+  .on('locationerror', (e) => {
+    console.log(e);
+    alert('Location access denied.');
+  });
