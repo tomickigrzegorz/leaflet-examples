a1c4e134c109a0469c5c0167594715ca2a78ccdb
Grzegorz Tomicki <gtomik@gmail.com>
2021-09-04 13:19:37 +0200

    feat: new example 'scale'

---
+++ 42.scale/script.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-undef */
+/**
+ * scale
+ */
+
+// config map
+let config = {
+  minZoom: 7,
+  maxZomm: 18,
+};
+// magnification with which the map will start
+const zoom = 18;
+// co-ordinates
+const lat = 52.22977;
+const lng = 21.01178;
+
+// calling map
+const map = L.map("map", config).setView([lat, lng], zoom);
+
+// Used to load and display tile layers on the map
+// Most tile servers require attribution, which you can set under `Layer`
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+// https://leafletjs.com/reference-1.7.1.html#control-scale
+L.control
+  .scale({
+    imperial: false,
+  })
+  .addTo(map);
