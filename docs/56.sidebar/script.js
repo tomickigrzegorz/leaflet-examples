1d298bad683cd6339a0da59a61ebff3ac9d32982
Grzegorz Tomicki <gtomik@gmail.com>
2022-02-07 10:50:41 +0100

    feat: sidebar without plugins

---
+++ docs/56.sidebar/script.js
@@ -0,0 +1,96 @@
+/* eslint-disable no-undef */
+/**
+ * sidebar
+ */
+
+// config map
+let config = {
+  minZoom: 7,
+  maxZoom: 18,
+};
+// magnification with which the map will start
+const zoom = 18;
+// co-ordinates
+const lat = 52.22977;
+const lng = 21.01178;
+
+// calling map
+const map = L.map("map", config).setView([lat, lng], zoom);
+
+// Used to load and display tile layers on the map
+// Most tile servers require attribution, which you can set under `Layer`
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+// --------------------------------------------------
+// sidebar
+
+const menuItems = document.querySelectorAll(".menu-item");
+const sidebar = document.querySelector(".sidebar");
+const buttonClose = document.querySelector(".close-button");
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    const target = e.target;
+
+    if (
+      target.classList.contains("active-item") ||
+      !document.querySelector(".active-sidebar")
+    ) {
+      document.body.classList.toggle("active-sidebar");
+    }
+
+    // show content
+    showContent(target.dataset.item);
+    // add active class to menu item
+    addRemoveActiveItem(target, "active-item");
+  });
+});
+
+// close sidebar when click on close button
+buttonClose.addEventListener("click", () => {
+  closeSidebar();
+});
+
+// remove active class from menu item and content
+function addRemoveActiveItem(target, className) {
+  const element = document.querySelector(`.${className}`);
+  target.classList.add(className);
+  if (!element) return;
+  element.classList.remove(className);
+}
+
+// show specific content
+function showContent(dataContent) {
+  const idItem = document.querySelector(`#${dataContent}`);
+  addRemoveActiveItem(idItem, "active-content");
+}
+
+// --------------------------------------------------
+// close when click esc
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    closeSidebar();
+  }
+});
+
+// close sidebar when click outside
+document.addEventListener("click", (e) => {
+  if (!e.target.closest(".sidebar")) {
+    closeSidebar();
+  }
+});
+
+// --------------------------------------------------
+// close sidebar
+
+function closeSidebar() {
+  document.body.classList.remove("active-sidebar");
+  const element = document.querySelector(".active-item");
+  const activeContent = document.querySelector(".active-content");
+  if (!element) return;
+  element.classList.remove("active-item");
+  activeContent.classList.remove("active-content");
+}
